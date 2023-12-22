00f7226c70f9cf8eef2a20a8cd3dd0ebf41a71dd
Ahmed Saman <98406575+Ahmedsam199@users.noreply.github.com>
2023-12-22 18:33:58 +0300

    Add UI

--- main.py
+++ main.py
@@ -1,55 +1,3 @@
-# from keras.models import load_model  # TensorFlow is required for Keras to work
-# import cv2  # Install opencv-python
-# import numpy as np
-#
-# # Disable scientific notation for clarity
-# np.set_printoptions(suppress=True)
-#
-# # Load the model
-# model = load_model("keras_Model.h5", compile=False)
-#
-# # Load the labels
-# class_names = open("labels.txt", "r").readlines()
-#
-# # CAMERA can be 0 or 1 based on default camera of your computer
-# camera = cv2.VideoCapture(0)
-#
-# while True:
-#     # Grab the webcamera's image.
-#     ret, image = camera.read()
-#
-#     # Resize the raw image into (224-height,224-width) pixels
-#     image = cv2.resize(image, (224, 224), interpolation=cv2.INTER_AREA)
-#
-#     # Show the image in a window
-#     cv2.imshow("Webcam Image", image)
-#
-#     # Make the image a numpy array and reshape it to the models input shape.
-#     image = np.asarray(image, dtype=np.float32).reshape(1, 224, 224, 3)
-#
-#     # Normalize the image array
-#     image = (image / 127.5) - 1
-#
-#     # Predicts the model
-#     prediction = model.predict(image)
-#     index = np.argmax(prediction)
-#     class_name = class_names[index]
-#     confidence_score = prediction[0][index]
-#
-#     # Print prediction and confidence score
-#     print("Class:", class_name[2:], end="")
-#     print("Confidence Score:", str(np.round(confidence_score * 100))[:-2], "%")
-#
-#     # Listen to the keyboard for presses.
-#     keyboard_input = cv2.waitKey(1)
-#
-#     # 27 is the ASCII for the esc key on your keyboard.
-#     if keyboard_input == 27:
-#         break
-#
-# camera.release()
-# cv2.destroyAllWindows()
-
 import os
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential
@@ -86,13 +34,12 @@ model.add(MaxPooling2D((2, 2)))
 model.add(Flatten())
 model.add(Dense(128, activation='relu'))
 model.add(Dense(len(classes), activation='softmax'))
-
+# ReLU(x)=max(0,x)  stands for Rectified Linear Unit. It is an activation function that introduces non-linearity to the model by outputting the input for positive values and zero for negative values.
 # Compile the model
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-
 # Train the model
 epochs = 10
 model.fit(train_generator, epochs=epochs, validation_data=validation_generator)
 
 # Save the model
-model.save('animal_classification_model.h5')
+model.save('animal_classification_model2.h5')
--- testing.py
+++ testing.py
@@ -1,29 +1,40 @@
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
+import tkinter as tk
+from tkinter import filedialog
+from PIL import Image, ImageTk
 import numpy as np
+from tensorflow.keras.models import load_model
 
 # Load the trained model
-model = load_model('animal_classification_model.h5')
-
+model = load_model('animal_classification_model2.h5')
+class_labels = ['dogs', 'cats']
 # Function to preprocess an image for prediction
 def preprocess_image(img_path):
-    img = image.load_img(img_path, target_size=(128, 128))
-    img_array = image.img_to_array(img)
+    img = Image.open(img_path).resize((128, 128))
+    img_array = np.array(img)
     img_array = np.expand_dims(img_array, axis=0)
-    img_array /= 255.0  # Normalize the pixel values
+    img_array = img_array / 255.0  # Normalize the pixel values
     return img_array
 
-# Example: Make predictions on a new image
-new_image_path = 'DogTest.jpg'
-preprocessed_image = preprocess_image(new_image_path)
+# Function to make predictions and update the label
+def predict_image():
+    file_path = filedialog.askopenfilename(title="Select Image", filetypes=[("Image files", "*.png;*.jpg;*.jpeg")])
+    if file_path:
+        preprocessed_image = preprocess_image(file_path)
+        predictions = model.predict(preprocessed_image)
+        predicted_class_index = np.argmax(predictions[0])
+        predicted_class = class_labels[predicted_class_index]
+        result_label.config(text=f'The model predicts that the image belongs to the class: {predicted_class}')
 
-# Make predictions
-predictions = model.predict(preprocessed_image)
+# Create the main window
+root = tk.Tk()
+root.title("Animal Classification")
 
-# Get the predicted class
-predicted_class_index = np.argmax(predictions[0])
-class_labels = ['dogs', 'cats']  # Make sure this list matches your original class labels
-predicted_class = class_labels[predicted_class_index]
+# Create and set up the widgets
+browse_button = tk.Button(root, text="Browse Image", command=predict_image)
+browse_button.pack(pady=20)
 
-# Display the prediction
-print(f'The model predicts that the image belongs to the class: {predicted_class}')
+result_label = tk.Label(root, text="")
+result_label.pack()
+
+# Start the Tkinter event loop
+root.mainloop()
